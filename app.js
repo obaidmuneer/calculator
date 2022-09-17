6bae50ae8b1b3f164b4e0c58e58a461006f3945b
obaidmuneer <obaidmuneer55@gmail.com>
2022-09-18 00:28:11 +0500

    calcualor 2.0

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-function sum() {
-    let num1 = +document.querySelector('#num1').value
-    let num2 = +document.querySelector('#num2').value
-    let result = num1+num2
-    console.log(result)
-    document.querySelector('#result').innerHTML = result
+function number(num){
+    let oldNum = document.querySelector('#screen').value
+    document.querySelector('#screen').value = oldNum + num
+}
+
+function deleteIt(){
+    // console.log(oldNum.substr(0,oldNum.length-1));
+    let oldNum = document.querySelector('#screen').value
+    extractedVal = oldNum.substr(0,oldNum.length-1)
+    document.querySelector('#screen').value = extractedVal
+
+}
+
+function calculate() {
+    let oldNum = document.querySelector('#screen').value
+    
+    // console.log(eval(oldNum))
+    document.querySelector('#screen').value = '='+eval(oldNum)
+    // document.querySelector('#result').innerHTML = result
 }
